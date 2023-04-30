8447b284a06c9df2d4be40c791988789ffd0c082
Berkay Mehmet Başkaya <110475301+berkaymbaskaya@users.noreply.github.com>
2023-04-30 20:17:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/firtApp/',
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./firtApp/src', import.meta.url))
+      '@': fileURLToPath(new URL('/firtApp./src', import.meta.url))
     }
   }
 })
